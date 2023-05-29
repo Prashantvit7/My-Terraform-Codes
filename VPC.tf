cc21539bfc39c18b555aed448619da9976a7ff90
Prashant Maurya <prashantvit7@gmail.com>
2023-05-29 10:54:52 +0000

    Complete Terraform Codes for netwroking DG

--- VPC.tf
+++ VPC.tf
@@ -1,73 +1,101 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  
+  cidr_block = "10.100.0.0/16"
   tags = {
-    Name = "NEW VPC by TERRAFORM"
+    Name = "upgrad-vpc"
   }
 }
-
 resource "aws_subnet" "public1" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.100.1.0/24"
 
   tags = {
-    Name = "Public Subnet1 T"
+    Name = "public-subnet-1"
   }
 }
-
 resource "aws_subnet" "public2" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.100.2.0/24"
 
   tags = {
-    Name = "Public Subnet2 T"
+    Name = "public-subnet-2"
   }
 }
-
 resource "aws_subnet" "private1" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.3.0/24"
+  cidr_block = "10.100.11.0/24"
 
   tags = {
-    Name = "Private Subnet1 T"
+    Name = "private-subnet-1"
   }
 }
-
 resource "aws_subnet" "private2" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.4.0/24"
+  cidr_block = "10.100.12.0/24"
 
   tags = {
-    Name = "Private Subnet2 T"
+    Name = "private-subnet-2"
   }
 }
-
-
-################ IGW
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "IGW by TERRAFORM"
+    Name = "upgrad-igw"
   }
 }
-
-############### Elatic IP
-
 resource "aws_eip" "eip" {
-  vpc=true
+  vpc      = true
 }
-
-##############  NAT Gateway
-
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.eip.id # It is the Elatic IP Allocation
+  allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public1.id
 
   tags = {
-    Name = "Terra NAT"
+    Name = "upgrad-nat"
   }
 
-  
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private.id
 }
